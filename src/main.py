bd7a7029db85d2fbb025a75cb512ffab1ed08aed
vbruttin <43440719+vbruttin@users.noreply.github.com>
2018-10-25 15:22:53 +0200

    first framework

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+
+from kivy.uix.floatlayout import FloatLayout
+
+
+class ShoulderTestApp(App):
+    def build(self):
+        f = FloatLayout()
+        return f
+
+
+
+
+
+if __name__ == '__main__':
+    ShoulderTestApp().run()
