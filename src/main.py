222db483fa8572290bc36c8707c6b31332fb5b6e
vbruttin <43440719+vbruttin@users.noreply.github.com>
2018-10-28 14:38:09 +0100

    4 pages, interactions

--- src/main.py
+++ src/main.py
@@ -8,37 +8,114 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 
 import random
-import time
+import re
+
+class Protocol(BoxLayout):
+    pass
 
 class ShoulderWindow(BoxLayout):
-    def display_text(self,angle):
-        self.ids.my_textinput.text = angle
+    
+    state = 0
+    state_list = ['Flexion', 'Abduction', 'External Rotation', 'Internal Rotation']
+
+    def __init__(self):
+        super(ShoulderWindow, self).__init__()
+        self.set_state()
+
+    def set_state(self):
+        self.ids.step_label.text = self.state_list[self.state]
+
+    def update_state(self, direction):
+        if direction == 'next':
+            self.state += 1
+            self.ids.back.disabled = False
+            if self.state == 3:
+                self.ids.next.disabled = True
+            self.ids.start_stop.disabled = False
+            self.ids.step_label.text = self.state_list[self.state]                
+            
+        elif direction == 'back':
+            self.state -= 1
+            self.ids.next.disabled = False
+            if self.state == 0:
+                self.ids.back.disabled = True
+            self.ids.start_stop.disabled = False   
+            self.ids.step_label.text = self.state_list[self.state]                
+             
+
+        else:
+            self.ids.state_label.text = 'Error'
+
+
+
+    def display_state(self,state):
+        self.ids.state_label.text = state
+
+    def display_angle(self, angle):
+        self.ids.angle_label.text = 'Angle : ' + angle + ' degrees'
 
     def get_angle(self):
         #angle =  dummy_function()
-        self.display_text(str(random.randint(0,180)))
+        self.display_angle(str(random.randint(0,180)))
 
     def measure(self):
         if self.ids.start_stop.text == 'Start':
-            self.display_text('Measurement')
+            self.display_state('Measurement')
             self.ids.start_stop.text = 'Stop'
 
         elif self.ids.start_stop.text == 'Stop':
+            self.display_state('Done')
             self.get_angle()
             self.ids.start_stop.text = 'Start'
             self.ids.start_stop.disabled = True
             self.ids.redo.disabled = False
             self.ids.validate.disabled = False
-
+            self.ids.overinput.disabled = False
         else:
-            self.display_angle('Error')
+            self.display_state('Error')
     
     def redo(self):
-        self.display_text('Input a value')
+        self.display_state('Cleared')
         self.ids.start_stop.text = 'Start'
         self.ids.start_stop.disabled = False
         self.ids.redo.disabled = True
         self.ids.validate.disabled = True
+        self.ids.overwrite.disabled = True
+        self.ids.overinput.disabled = True
+        self.ids.overinput.text = 'Overwrite'
+
+
+    def overinput(self):
+        if re.match('^[0-9]+$', self.ids.overinput.text):
+            self.ids.overwrite.disabled = False
+        else:
+            self.ids.overwrite.disabled = True
+    
+    def overwrite(self):
+        self.display_angle(self.ids.overinput.text)
+
+    def validate(self):
+        self.display_state('Validated')
+        self.ids.redo.disabled = True
+        self.ids.validate.disabled = True
+        self.ids.overwrite.disabled = True
+        self.ids.overinput.disabled = True
+        self.ids.overinput.text = 'Overwrite'
+
+        if self.state == 0:
+            self.ids.table.ids.flexion.text = self.ids.angle_label.text[8:-8]
+        elif self.state == 1:
+            self.ids.table.ids.abduction.text = self.ids.angle_label.text[8:-8]
+        elif self.state == 2:
+            self.ids.table.ids.ext_rot.text = self.ids.angle_label.text[8:-8]
+        elif self.state == 3:
+            self.ids.table.ids.int_rot.text = self.ids.angle_label.text[8:-8]
+        else: 
+            self.ids.state_label.text == 'Error'
+        
+        self.ids.angle_label.text = 'Angle :'
+
+
 
         
 
