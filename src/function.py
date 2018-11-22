0a4a1072a6cc6fe8731ef3fea07472b2b9cd1d5c
giustogianni <gianni.giusto@epfl.ch>
2018-11-22 20:43:24 +0100

    BIG DATA IS COMING

--- src/function.py
+++ src/function.py
@@ -1,20 +1,20 @@
-from math import sqrt
 import numpy as np
 import csv
 import pandas as pd
-from pykalman import KalmanFilter
+
+fileLoc = '../data/F_H_angle3_5.csv'
 
 def computeAngle(v1, v2):
     angle = (180/np.pi)*np.arccos(sum(v1*v2)/(np.linalg.norm(v1)*np.linalg.norm(v2)))
     return angle
 
 def getStartVector():
-    data = pd.read_csv('../data/F_G_40_5.csv',header = 4, skiprows = [6], usecols = [4,5,6])
+    data = pd.read_csv(fileLoc, header = 4, skiprows = [6], usecols = [4,5,6])
     startVector = data.iloc[0:128].median()
     return startVector
 
 def getEndVector():
-    data = pd.read_csv('../data/F_G_40_5.csv',header = 4, skiprows = [6], usecols = [4,5,6])
+    data = pd.read_csv(fileLoc, header = 4, skiprows = [6], usecols = [4,5,6])
     endVector = data.iloc[-256:].median()
     return endVector
 
