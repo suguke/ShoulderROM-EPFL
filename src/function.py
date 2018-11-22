bb207959d7bdf547a41dd86ba282582f9746b760
juju2147 <44406920+juju2147@users.noreply.github.com>
2018-11-22 18:07:00 +0100

    Add files via upload

--- src/function.py
+++ src/function.py
@@ -2,42 +2,26 @@ from math import sqrt
 import numpy as np
 import csv
 import pandas as pd
-
-# class vector():
-#     def __init__(self):
-#         self.acc = np.zeros((3,1),dtype ='i')
-#         self.gyr = np.zeros((3,1),dtype ='i')
-    
-#     def computeNorm(self):
-#         norm = np.sqrt(np.sum(np.power(self.acc,2)))
-#         return norm
-
-# def computeScalar(v1, v2):
-#     scalar = np.sum(np.multiply(v1.acc,v2.acc))
-#     return scalar
-
-# def computeAngle(v1, v2):
-#     norm1 = v1.computeNorm()
-#     norm2 = v2.computeNorm()
-#     angle = np.arccos(computeScalar(v1,v2)/(norm1*norm2))
-#     return angle
+from pykalman import KalmanFilter
 
 def computeAngle(v1, v2):
-    angle = np.arccos((180/np.pi)*np.arccos(sum(v1*v2)/(np.linalg.norm(v1)*np.linalg.norm(v2))))
+    angle = (180/np.pi)*np.arccos(sum(v1*v2)/(np.linalg.norm(v1)*np.linalg.norm(v2)))
     return angle
 
 def getStartVector():
-    data = pd.read_csv('data.csv',header = 4, skiprows = [6], usecols = [4,5,6])
+    data = pd.read_csv('../data/F_G_40_5.csv',header = 4, skiprows = [6], usecols = [4,5,6])
     startVector = data.iloc[0:128].median()
     return startVector
 
 def getEndVector():
-    data = pd.read_csv('data.csv',header = 4, skiprows = [6], usecols = [4,5,6])
+    data = pd.read_csv('../data/F_G_40_5.csv',header = 4, skiprows = [6], usecols = [4,5,6])
     endVector = data.iloc[-256:].median()
     return endVector
 
-# x = vector()
-# y = vector()
-# x.acc = [1,55,0]
-# y.acc = [32,0,1]
-# print((180/np.pi)*computeAngle(x,y))
+#kf = KalmanFilter(transition_matrices = [[1, 1], [0, 1]], observation_matrices = [[0.1, 0.5], [-0.3, 0.0]]),measurements = np.asarray([[1,0], [0,0], [0,1]])
+startVect = getStartVector()
+endVect = getEndVector()
+print(startVect)
+print(endVect)
+angle = computeAngle(startVect,endVect)
+print(angle)
